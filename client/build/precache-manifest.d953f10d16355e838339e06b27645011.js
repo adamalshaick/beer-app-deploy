bca4df37cfa8fb1324e6ee6d76ad2e295cc54197
unknown <adamalshaick@gmail.com>
2019-04-07 18:03:03 +0200

    build

---
+++ client/build/precache-manifest.d953f10d16355e838339e06b27645011.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdc0a3b817af7fd523a3",
+    "url": "/BeerApp/static/js/runtime~main.fdc0a3b8.js"
+  },
+  {
+    "revision": "dd9f0a61f2864fcc127f",
+    "url": "/BeerApp/static/js/main.dd9f0a61.chunk.js"
+  },
+  {
+    "revision": "dba99e41d0162ec621c3",
+    "url": "/BeerApp/static/js/2.dba99e41.chunk.js"
+  },
+  {
+    "revision": "14a5e63fbbd91c4d3f75411d067995eb",
+    "url": "/BeerApp/index.html"
+  }
+];
